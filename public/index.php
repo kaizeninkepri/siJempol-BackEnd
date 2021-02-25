da7db789180a52870851b69ed6895b82e9b8e0d7
websturing <websturing.project@gmail.com>
2021-02-25 14:00:33 +0700

    remove header sent in index.php

--- public/index.php
+++ public/index.php
@@ -53,5 +53,3 @@ $response = tap($kernel->handle(
 ))->send();
 
 $kernel->terminate($request, $response);
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
