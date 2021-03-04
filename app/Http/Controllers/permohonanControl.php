b9b6b845ffe7f05672a858b08095390ce6d6cc33
websturing <websturing.project@gmail.com>
2021-03-04 15:09:39 +0700

    upload via mobile

--- app/Http/Controllers/permohonanControl.php
+++ app/Http/Controllers/permohonanControl.php
@@ -285,7 +285,7 @@ class permohonanControl extends Controller
         $filename = $persyaratan->permohonan_persyaratanId . '-' . $name . '.' . $file->getClientOriginalExtension();
 
         $path = Storage::disk("ResourcesExternal")->path($perusahaan->npwp . '/' . $permohonan->permohonan_code . '/persyaratan' . '/' . $filename);
-        file_put_contents($path, $filename);
+        file_put_contents($path, $file->getClientOriginalName());
 
         $arPers = array(
             "status" => "uploaded",
